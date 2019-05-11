73c191c46a285cc4f700c40147ecf247031529b3
issuy <akito.ew@gmail.com>
2019-05-12 00:07:59 +0900

    No need to purge.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,6 @@ class ItemsController < ApplicationController
   # PATCH/PUT /items/1.json
   def update
     respond_to do |format|
-      @item.image.purge if params[:image]
       if @item.update(item_params)
         format.html { redirect_to @item, notice: 'Item was successfully updated.' }
         format.json { render :show, status: :ok, location: @item }
@@ -56,7 +55,6 @@ class ItemsController < ApplicationController
   # DELETE /items/1
   # DELETE /items/1.json
   def destroy
-    @item.image.purge
     @item.destroy
     respond_to do |format|
       format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
